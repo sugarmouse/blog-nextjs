4d7eef98f03d8268a930ecdeacf00feb08f0d1d9
sugarmouse <tanghao19950911@gmail.com>
2022-11-01 13:36:36 +0800

    lib/post add getPost 函数

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -2,16 +2,18 @@ import fs, { promises as fsPromise } from 'fs';
 import path from "path";
 import matter from 'gray-matter';
 
+const MARKDOWN_DIR = path.join(process.cwd(), 'markdown');
+
+
 /**
  * It reads the markdown directory, gets the file names, and then maps over the file names to create an
  * array of objects that contain the id, title, and date of each post
  * @returns An array of objects with the id, title, and date of each post.
  */
 const getPosts = async () => {
-  const markdownDir = path.join(process.cwd(), 'markdown');
-  const fileNames = await fsPromise.readdir(markdownDir);
+  const fileNames = await fsPromise.readdir(MARKDOWN_DIR);
   const posts = fileNames.map(fileName => {
-    const fullPath = path.join(markdownDir, fileName);
+    const fullPath = path.join(MARKDOWN_DIR, fileName);
     const id = fileName.replace(/\.md$/g, '');
     const text = fs.readFileSync(fullPath, 'utf-8');
     const { data: { title, date }, content } = matter(text);
@@ -21,3 +23,16 @@ const getPosts = async () => {
 };
 
 export default getPosts;
+
+
+export const getPost = async (id: string) => {
+  const fullPath = path.join(MARKDOWN_DIR, id + '.md');
+  const text = fs.readFileSync(fullPath, 'utf-8');
+  const { data: { title, date }, content } = matter(text);
+  return { id, title, date, content };
+};
+
+export const getPostIds = async () => {
+  const fileNames = await fsPromise.readdir(MARKDOWN_DIR);
+  return fileNames.map(fileName => fileName.replace(/\.md$/g, ''));
+};
