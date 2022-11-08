348b7d404f69ab5f9d3e150c34e471e722de00d9
sugarmouse <tanghao19950911@gmail.com>
2022-11-08 23:06:03 +0800

    init typeorm

---
+++ src/data-source.ts
@@ -0,0 +1,23 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+// import { User } from "./entity/User" 
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "test",
+    password: "test",
+    database: "test",
+    synchronize: true,
+    logging: false,
+    entities: [
+
+    ],
+    migrations: [
+
+    ],
+    subscribers: [
+
+    ],
+});
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { AppDataSource } from "./data-source";
+import { User } from "./entity/User";
+
+AppDataSource.initialize().then(async (connection) => {
+    console.log("Inserting a new user into the database...");
+    console.log(connection);
+    connection.destroy();
+
+
+
+    // const user = new User()
+    // user.firstName = "Timber"
+    // user.lastName = "Saw"
+    // user.age = 25
+    // await AppDataSource.manager.save(user)
+    // console.log("Saved a new user with id: " + user.id)
+
+    // console.log("Loading users from the database...")
+    // const users = await AppDataSource.manager.find(User)
+    // console.log("Loaded users: ", users)
+
+    // console.log("Here you can setup and run express / fastify / any other framework.")
+
+}).catch(error => console.log(error));
